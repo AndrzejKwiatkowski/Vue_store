9bba59e4aae7e1b4430af3a599098ce50ac66acc
AndrzejKwiatkowski <andrzej_kwiatkowski@outlook.com>
2020-01-28 07:27:38 +0100

    Added components: Login, Article; check authorization with token

---
+++ src/components/Articele.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+<navbar/>
+<div class="container">
+<div class="card d-table-cell" style="width: 18rem;" v-for="(articele, index) in articeles" :key="index">
+  <img class="card-img-top" :src="'img/' + articele.image" alt="Card image cap">
+  <div class="card-body" >
+    <h5 class="card-title">Clothes</h5>
+    <p class="card-text">
+      
+            Number: {{ articele.id }} <br/>
+            Gender: {{ articele.gender }}<br/>
+            Type: {{ articele.type }}<br/>
+            Size: {{ articele.size }}<br/>
+            Price: {{ articele.price }}<br/>
+        
+    </p>
+    <a href="#" class="btn btn-primary">Add basket</a>
+    </div>
+</div>
+</div>
+</div>
+
+</template>
+
+<script>
+import axios from 'axios'
+import Navbar from '../components/Navbar'
+
+export default {
+    name: 'Articele',
+    components:{
+        Navbar,
+    },
+    data() {
+        return {
+            articeles: ''
+        }
+    },
+
+    created() {
+        axios.get('http://localhost:8000/api/articeles')
+        .then((response) => {this.articeles = response.data
+        console.log(this.articeles)
+        }
+        )
+        
+    }
+
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,67 @@
+<template>
+<form class="container col-md-4 mt-5">
+  <div class="form-group" >
+    <label for="exampleInputEmail1">Email address</label>
+    <input v-model="email" type="email"  class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <button type="submit" @click.prevent="login" class="btn btn-primary">Submit</button>
+
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+import router from '../router/index'
+
+export default {
+  name: 'Login',
+  
+  data() { 
+      return {
+        email: '',
+        password: '',
+      }
+    },
+
+  methods: {
+    
+    login () {
+        axios.post('http://localhost:8000/api/login', { email: this.email, password: this.password })
+    .then((response) => 
+             {
+                localStorage.setItem('token', response.data.token)
+
+               if(localStorage.token) {
+                 router.push({path: '/home'})
+               }
+               else{
+                 router.push({path: '/login'})
+               }
+                              
+               
+  })
+  
+  
+
+
+   
+           
+             }
+              
+               
+            
+            
+
+    }
+}
+
+</script>
+
+<style scoped lang="scss">
+</style>
